64b1f7fbd4ca1fce5b7a1587585b94abf4e1649d
JamesJuta <116991901+JamesJuta@users.noreply.github.com>
2023-11-25 15:47:41 +0700

    Add files via upload

---
+++ noli/nobela.php
@@ -0,0 +1,279 @@
+<div class="tab-content">
+<div id="nobela" data-tab-content>
+                <form class="nobela_form container" action="" method="post">
+                    <h1 class="katanungan_title title"><i class='bx bx-face icon' ></i> Mga Tauhan</h1>
+
+                    <!-- <div class="arrow-container">
+                        <i data-tab-target="#kabanata_64" class='bx bx-chevron-left'></i>
+                        <i data-tab-target="#kabanata_1" class='bx bx-chevron-right'></i>
+                    </div> -->
+
+                    <div class="nobela_box_container">
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Ibarra.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Juan Crisóstomo Ibarra y Magsalin</h1>   
+                                <p>Si Crisóstomo Ibarra ay nangarap makapagpatayo ng paaralan upang matiyak ang magandang kinabukasan ng mga kabataan ng San Diego. Nag-iisa siyang anak ni Don Rafael Ibarra. Siya ay katipan ni Maria Clara.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Maria_Clara.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Maria Clara</h1>
+                                <p>Ang babaeng pinakamamahal ni Ibarra. Kilala si Maria Clara sa San Diego dahil sa kanyang angking ganda at kayumian. Anak siya ni Donya Pia Alba kay Padre Damaso.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Kapitan_Tiyago.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Kapitan Tiyago o Don Santiago de los Santos</h1>
+                                <p>Mangangalakal na taga-Binondo. Siya ang ama-amahan ni Maria Clara. Madalas siyang magpahanda ng salu-salo at kilala sa pagiging bukas-palad.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Padre_Damaso.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Padre Damaso Verdolagas</h1>
+                                <p>Isang kurang Pransiskano na masalita at talagang magaspang kumilos. Siya ay dating kura sa San Diego na nagpahukay at nagpalipat ng bangkay ni Don Rafael Ibarra sa libingan ng mga hindi banyaga.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Padre_Salvi.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Padre Bernardo Salvi</h1>
+                                <p>Ang paring pumalit kay Padre Damaso na mayroong lihim na pagtingin kay Maria Clara.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Padre_Hernando.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Padre Hernando De La Sibyla</h1>
+                                <p>Paring Agustino na lihim na sumusubaybay sa mga kilos ni Ibarra.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Pilosopo_Tasyo.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Pilosopo Tasyo o Don Anastacio</h1>
+                                <p>Matandang tagapayo ng marurunong na mamamayan ng San Diego. Ang tingin ng mga ‘di nakapag-aral ay baliw ang matanda ngunit para sa mga may alam ay isa siyang pilosopo. Hindi siya nakapagpatuloy ng pag-aaral at maagang nabalo kaya ginugol ang panahon sa pagbabasa ng mga aklat.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Sisa.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Sisa</h1>
+                                <p>Isang mapagmahal na ina na nabaliw. Siya ay may asawang pabaya at malupit.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Basilio_Crispin.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Basilio at Crispin</h1>
+                                <p>Magkapatid na anak ni Sisa. Mga napagbintangang nagnakaw. Pareho silang sakristan at tagatugtog ng kampana sa simbahan ng San Diego. Si Basilio ang panganay at bunso naman si Crispin.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Pedro.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Pedro</h1>
+                                <p>Iresponsableng ama at masamang asawa ni Sisa.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Kapitan_Heneral.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Kapitan-Heneral</h1>
+                                <p>Siya ang pinaka makapangyarihang opisyal ng Espanya sa Pilipinas. Siya ang lumakad upang maalisan ng pagka-ekskomunyon si Ibarra.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Tinyente_Guevarra.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Tinyente Guevarra</h1>
+                                <p>Isang matapat na tinyente ng mga guwardiya sibil na nagsalaysay kay Ibarra ng tungkol sa kasawiang sinapit ng kanyang ama.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Don_Rafael.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Don Rafael Ibarra</h1>
+                                <p>Ama ni Crisostomo Ibarra. Labis siyang kinainggitan ni Padre Damaso dahil mayaman ito. Tinawag din siyang erehe.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Don_Pedro.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Don Pedro Eibarrimendia</h1>
+                                <p>Lolo ni Crisostomo Ibarra. Siya ang naging dahilan ng matinding kasawian ng nuno ni Elias.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Don_Saturnino.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Don Saturnino</h1>
+                                <p>Ninuno ni Crisostomo Ibarra.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Don_Filipo.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Don Filipo</h1>
+                                <p>Tinyente mayor na mahilig magbasa na Latin. Siya ang ama ni Sinang.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Don_Primitivo.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Don Primitivo</h1>
+                                <p>Isang matandang lalaki na marunong mangatwiran at mahilig magsalita ng Latin. Siya ay pinsan ni Tinchang at taga payo ni Kapitan Tinong.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Don_Tiburcio.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Don Tiburcio de Espadaña</h1>
+                                <p>Isang pilay at bungal na Kastilang napadpad sa Pilipinas sa paghahanap ng magandang kapalaran. Asawa ni Donya Victorina at nagpanggap na doktor.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Donya_Victorina.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Donya Victorina de los Reyes de Espadaña</h1>
+                                <p>Babaeng nagpapanggap na mestisang Kastila kung kaya abut-abot ang kolorete sa mukha at maling pangangastila. Asawa ni Don Tiburcio de Espadaña</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Donya_Consolacion.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Donya Consolacion</h1>
+                                <p>Napangasawa ng Alperes. Dati siyang labandera na may malaswang bibig at pag-uugali. Katawa-tawa siya kung manamit at ikinahihiyang isama ng Alperes. Ipinapalagay niya na siya’y higit na maganda kay Maria Clara.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Donya_Pia.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Donya Pia Alba</h1>
+                                <p>Masimbahing ina ni Maria Clara na namatay matapos na maisilang ang anak.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Tiya_Isabel.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Tiya Isabel</h1>
+                                <p>Pinsan ni Kapitan Tiyago na tumulong sa pagpapalaki kay Maria Clara.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Alperes.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Alperes</h1>
+                                <p>Siya ang puno ng mga gwardya sibil at kaagaw ng kura sa kapangyarihan sa San Diego. Asawa niya si Donya Consolacion.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Linares.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Linares</h1>
+                                <p>Malayong pamangkin ni Don Tiburcio at pinsan ng inaanak ni Padre Damaso na napili niya para mapangasawa ni Maria Clara.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Kapitana_Maria.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Kapitana Maria</h1>
+                                <p>Ang nag-iisang babaeng makabayan na pumanig sa pagtatanggol ni Ibarra sa alaala ng ama.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="nobela_card container">
+                            <img src="images/noli/Sinang.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Sinang</h1>
+                                <p>Anak ni Kapitan Basilio at masayahing kaibigan ni Maria Clara.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Andeng.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Andeng</h1>
+                                <p>Mahusay magluto na kinakapatid ni Maria Clara.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Victoria.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Victoria</h1>
+                                <p>Tahimik na kaibigan ni Maria Clara na kaintahan ni Albino.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Neneng.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Neneng</h1>
+                                <p>Isa sa mga kaibigan ni Maria Clara. Siya ay mahinhin at palaisip.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Iday.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Iday</h1>
+                                <p>Kaibigang maganda ni Maria Clara na tumutugtog ng alpa.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Senyor_Nol_Juan.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Senyor Nol Juan</h1>
+                                <p>Siya ang namahala ng mga gawain sa pagpapatayo ng paaralan.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Lucas.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Lucas</h1>
+                                <p>Taong dilaw na gumawa ng kalong ginamit sa di-natuloy na pagpatay kay Ibarra.</p>
+                            </div>
+                        </div>
+
+                        <div class="nobela_card container">
+                            <img src="images/noli/Mang_Pablo.png" class="nobela_profile_img">
+                            <div class="content">
+                                <h1>Mang Pablo</h1>
+                                <p>Matandang pinuno ng mga tulisan na ibig tulungan ni Elias.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
